99259c6ec293c2afa0ff4baed38f5afcf4c20428
Robert McGibbon <rmcgibbo@gmail.com>
2014-02-23 19:00:09 -0800

    use distutils instead of setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 """
 setup.py: Install simtk.units
 """
+from distutils.core import setup
 try:
-    from setuptools import setup
+   from distutils.command.build_py import build_py_2to3 \
+        as build_py
 except ImportError:
-    from distutils.core import setup
+   from distutils.command.build_py import build_py
 
 major_version_num='0'
-minor_version_num='1'
+minor_version_num='2'
 
 def doSetup():
     setupKeywords = {}
@@ -15,12 +17,12 @@ def doSetup():
     setupKeywords["version"]           = "%s.%s" % (major_version_num, minor_version_num)
     setupKeywords["author"]            = "Christopher M. Bruns"
     setupKeywords["author_email"]      = "cmbruns@stanford.edu"
+    setupKeywords["cmdclass"]          = {'build_py': build_py}
     setupKeywords["url"]               = "https://github.com/rmcgibbo/simtk.unit"
     setupKeywords["license"]           = "Python Software Foundation License (BSD-like)"
     setupKeywords["packages"]          = ["simtk", "simtk.unit"]
     setupKeywords["data_files"]        = []
     setupKeywords["platforms"]         = ["Linux", "Mac OS X", "Windows"]
-    setupKeywords["use_2to3"]          = True
     setupKeywords["description"]       = "Units for python"
     setupKeywords["long_description"]  = \
     """Units-aware quantity classes for python
